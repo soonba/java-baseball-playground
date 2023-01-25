87f07a8300b89581c5e992ac4a25f16265cbf9bd
sunba <sunba2318@gmail.com>
2023-01-25 21:32:07 +0900

    test : 문자열 계산기

---
+++ src/test/java/study/StringCalculatorTest.java
@@ -0,0 +1,30 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import stringCalculator.StringCalculator;
+
+public class StringCalculatorTest {
+
+    private static StringCalculator stringCalculator;
+
+    @BeforeAll
+    static void setUpAll() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    void calculate1() {
+        String[] strArr = "2 + 3 * 4 / 2".split(" ");
+        int calculate = stringCalculator.calculate(strArr);
+        Assertions.assertThat(calculate).isEqualTo(10);
+    }
+
+    @Test
+    void calculate2() {
+        String[] strArr = "2 * 3 - 4 / 2".split(" ");
+        int calculate = stringCalculator.calculate(strArr);
+        Assertions.assertThat(calculate).isEqualTo(1);
+    }
+}
