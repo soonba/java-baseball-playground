ab7f11b28496a649d3e82431f3fa63e7b7eab4d9
sunba <sunba2318@gmail.com>
2023-02-10 14:36:01 +0900

    feat : ball 비교 메서드 play() 생성

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -2,13 +2,15 @@ package baseball;
 
 public class Ball {
     private final int value;
+    private final int location;
 
-    public Ball(int num) {
-        this.value = validate(num);
+    public Ball(int location, int num) {
+        this.location = location;
+        this.value = validateValue(num);
     }
 
-    private static int validate(int num) {
-        if(!(num >= 1 && num <= 9)) {
+    private static int validateValue(int num) {
+        if (!(num >= 1 && num <= 9)) {
             throw new IllegalArgumentException("값은 1부터 9사이 여야합니다.");
         }
         return num;
@@ -17,4 +19,23 @@ public class Ball {
     public int getValue() {
         return value;
     }
+
+    public BallStatus play(Ball userBall) {
+        if (this.equals(userBall)) {
+            return BallStatus.STRIKE;
+        }
+        if (this.getValue() == userBall.getValue()) {
+            return BallStatus.BALL;
+        }
+        return BallStatus.NOTHING;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ball ball = (Ball) o;
+        return value == ball.value && location == ball.location;
+    }
+
 }
--- src/test/java/baseball/BallTest.java
+++ src/test/java/baseball/BallTest.java
@@ -9,17 +9,38 @@ public class BallTest {
 
     @Test
     void 입력은_1에서_9사이() {
-        assertThatThrownBy(() -> new Ball(0)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Ball(0,0)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 입력은_1에서_9사이2() {
-        assertThatThrownBy(() -> new Ball(10)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Ball(0,10)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 입력은_1에서_9사이3() {
-        Ball ball = new Ball(5);
+        Ball ball = new Ball(0,5);
         assertThat(ball.getValue()).isEqualTo(5);
+    }
+
+    @Test
+    void 볼_스트라이크() {
+        Ball ball = new Ball(0,1);
+        Ball userBall = new Ball(0,1);
+        assertThat(ball.play(userBall)).isEqualTo(BallStatus.STRIKE);
+    }
+
+    @Test
+    void 볼_볼() {
+        Ball ball = new Ball(0,1);
+        Ball userBall = new Ball(2,1);
+        assertThat(ball.play(userBall)).isEqualTo(BallStatus.BALL);
+    }
+
+    @Test
+    void 볼_낫싱() {
+        Ball ball = new Ball(0,1);
+        Ball userBall = new Ball(2,5);
+        assertThat(ball.play(userBall)).isEqualTo(BallStatus.NOTHING);
     }
 }
