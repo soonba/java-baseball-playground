ff56bd5d77270cf7d027f5534e355a8137f6effd
sunba <sunba2318@gmail.com>
2023-02-14 22:47:00 +0900

    feat : balls, balls test 추가

---
+++ src/main/java/baseball/Balls.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Balls {
+    private final List<Ball> ballList;
+
+    public Balls(List<Ball> ballList) {
+        this.ballList = ballList;
+    }
+
+
+    public BallStatus play(Ball comBall) {
+
+        List<BallStatus> ballStatusList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            ballStatusList.add(ballList.get(i).play(comBall));
+        }
+        return ballStatusList.stream().filter(ballStatus -> !ballStatus.isNothing())
+                .findFirst()
+                .orElse(BallStatus.NOTHING);
+    }
+
+}
---
+++ src/test/java/baseball/BallsTest.java
@@ -0,0 +1,45 @@
+package baseball;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BallsTest {
+
+    private static Balls comBalls;
+
+    @BeforeEach
+    void setup() {
+        List<Ball> ballList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            ballList.add(new Ball(i + 1, i + 2));
+        }
+        comBalls = new Balls(ballList);
+    }
+
+    @Test
+    void 볼() {
+        Ball ball = new Ball(1, 3);
+        BallStatus ballStatus = comBalls.play(ball);
+        assertThat(ballStatus).isEqualTo(BallStatus.BALL);
+    }
+
+    @Test
+    void 스트라이크() {
+        Ball ball = new Ball(3, 4);
+        assertThat(comBalls.play(ball)).isEqualTo(BallStatus.STRIKE);
+    }
+
+    @Test
+    void 낫싱() {
+        Ball ball = new Ball(3, 8);
+        BallStatus play = comBalls.play(ball);
+        assertThat(play).isEqualTo(BallStatus.NOTHING);
+    }
+
+
+}
