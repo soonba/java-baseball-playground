f396118da686baf798f9333b0c85318d614c25fe
sunba <sunba2318@gmail.com>
2023-02-17 18:38:51 +0900

    style : Random Balls 생성 유틸클래스 스타일

--- src/main/java/baseball/RandomBallsGenerator.java
+++ src/main/java/baseball/RandomBallsGenerator.java
@@ -1,17 +1,16 @@
 package baseball;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class RandomBallsGenerator {
 
     static int nowNum;
     static List<Ball> ballList = new ArrayList<>();
     static List<Integer> numList = new ArrayList<>();
+
     static Balls apply() {
-        while(numList.size() < 3) {
+        while (numList.size() < 3) {
             nowNum = (int) ((Math.random() * 9) + 1);
             addIfNewNumber();
         }
@@ -20,8 +19,8 @@ public class RandomBallsGenerator {
     }
 
     private static void addIfNewNumber() {
-        if(!numList.contains(nowNum)) {
-            ballList.add(new Ball(numList.size()+1,nowNum));
+        if (!numList.contains(nowNum)) {
+            ballList.add(new Ball(numList.size() + 1, nowNum));
             numList.add(nowNum);
         }
     }
