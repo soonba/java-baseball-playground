cbc89c98df2956969eb896d138bd7e49cad8acbd
sunba <sunba2318@gmail.com>
2023-02-25 19:53:27 +0900

    refactor : 결과 출력 리팩토링

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -1,23 +1,31 @@
 package baseball.domain;
 
 import baseball.utils.RandomBallsGenerator;
+import baseball.view.ErrorView;
 import baseball.view.InputView;
 import baseball.view.OutputView;
-
-import java.io.IOException;
 
 public class Game {
     private final Balls comBalls = RandomBallsGenerator.apply();
     private final Baseball baseball = new Baseball(comBalls);
 
-    public void start() throws IOException {
+    public void start() {
         OutputView.printStartMessage();
         while (!baseball.endGame()) {
-            OutputView.printInputRequestMessage();
-            String read = InputView.readNumber();
-            PlayResult aResult = baseball.play(Baseball.mapToBallList(read));
+            PlayResult aResult = tryPlaying();
             OutputView.printResult(aResult);
         }
         OutputView.printEndMessage();
+    }
+
+    private PlayResult tryPlaying() {
+        try {
+            OutputView.printInputRequestMessage();
+            String read = InputView.readNumber();
+            return baseball.play(Baseball.mapToBallList(read));
+        } catch (Exception e) {
+            ErrorView.print(e.getMessage());
+            return tryPlaying();
+        }
     }
 }
---
+++ src/main/java/baseball/view/ErrorView.java
@@ -0,0 +1,7 @@
+package baseball.view;
+
+public class ErrorView {
+    public static void print(String message) {
+        System.out.println(message);
+    }
+}
